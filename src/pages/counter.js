ddbfa6997f59a390e8951de79e5fb734a3416873
Mike Polinowski <mpolinowski@users.noreply.github.com>
2017-10-01 21:49:01 +0800

    Update

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class Counter extends React.Component {
   constructor() {
@@ -7,12 +8,19 @@ class Counter extends React.Component {
   }
   render() {
     return <div>
-            <h1>Counter</h1>
+            <h3 style={{color: this.props.color}}>{this.props.header}</h3>
             <p>current count: {this.state.count}</p>
             <button onClick={() => this.setState({ count: this.state.count + 1 })}>plus</button>
             <button onClick={() => this.setState({ count: this.state.count - 1 })}>minus</button>
           </div>
   }
+
+}
+
+// Specifies the default values for props:
+Counter.defaultProps = {
+header: 'Default Counter',
+color: 'black'
 }
 
 export default Counter
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import Counter from './counter'
+
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to Page 2</Link><br/>
-    <Link to="/counter/">Go to Counter</Link>
+    <Link to="/page-2/">Go to Page 2</Link><br/><br/>
+    <Counter header="This is the Index Counter" color="rebeccapurple" />
   </div>
 )
 
